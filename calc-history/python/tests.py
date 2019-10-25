05d62a3f5e24ebc7a0f4d6fb53a793a5a13f6d14
Eric Ames <amesen07@gmail.com>
2019-10-24 21:34:05 -0700

    Initial commit. Adding dir tree for calc-history and misc.

---
+++ calc-history/python/tests.py
@@ -0,0 +1,20 @@
+""" Functional tests for Python Calculator ( calc_main) """
+
+
+import calc_main
+
+
+class SetupCalculator():
+
+    def setup():
+        #TODO
+        print( "TODO")
+
+
+
+
+class ArithmeticTest():
+
+    def test_addition():
+        #TODO
+        print( "TODO")
